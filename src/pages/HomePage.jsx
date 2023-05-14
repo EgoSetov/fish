f8b5cfc62ad85aef962eb1c13800233a0c20650c
Егор <egosetov@MacBook-Pro-Egor.local>
2023-05-14 11:33:55 +0300

    added info to rules page and home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,31 @@
-import { Container } from "react-bootstrap";
+import { Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <Container className="mt-3">
       <h2>Главная страница</h2>
+      <div className="my-3">
+        <h3>Добро пожаловать на рабаловный форум</h3>
+      </div>
+      <h5>Разделы, которые вы можете посетить</h5>
+      <ul>
+        <li>
+          <h6>
+            <Link to="posts">Форум</Link>
+          </h6>
+        </li>
+        <li>
+          <h6>
+            <Link to="about">О нас</Link>
+          </h6>
+        </li>
+        <li>
+          <h6>
+            <Link to="rules">Правила</Link>
+          </h6>
+        </li>
+      </ul>
     </Container>
   );
 };
